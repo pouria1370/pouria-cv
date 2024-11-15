caf517d3157f4fa11afd8a48e1bd073da006c124
pouria1370 <kalantari_pouria@yahoo.com>
2024-11-15 19:22:10 +0330

    feat:the first animating with gsap and first integrating with prismitic

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Pouria Kalantari",
+  description: "Frontend developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} text-slate-100 bg-slate-900 antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
